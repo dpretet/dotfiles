4bc864a107866d4f45a899ccd737d7b6916e8b4a
Damien <dosadih@gmail.com>
2017-03-09 20:58:24 +0100

    Update template.v

--- .vim/templates/template.v
+++ .vim/templates/template.v
@@ -1,6 +1,5 @@
 //-----------------------------------------------------------------------
-
-Copyright 2017 Damien Pretet
+Copyright 2017 Damien Pretet ThotIP
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
