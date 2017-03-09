d7e697f89f6d370ff996692c197f779533dc72fa
Damien <dosadih@gmail.com>
2017-03-09 20:59:32 +0100

    Update template.v

--- .vim/templates/template.v
+++ .vim/templates/template.v
@@ -1,20 +1,20 @@
 //-----------------------------------------------------------------------
-Copyright 2017 Damien Pretet ThotIP
+// Copyright 2017 Damien Pretet ThotIP
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //-----------------------------------------------------------------------
 
-`timescale 1ns/1ps
+`timescale 1 ns / 1 ps
 `default_nettype none
 
 // Synthesis
@@ -23,8 +23,8 @@ module ModuleName
 
     #(
     )(
-    input wire  clk,
-    input wire  rstn
+    input wire  aclk,
+    input wire  arstn
     );
 
 endmodule
@@ -35,8 +35,8 @@ endmodule
 
 module tb_name;
 
-    reg         clk;
-    reg         rstn;
+    reg         aclk;
+    reg         arstn;
     integer     clk_period = 10;
 
     initial begin
